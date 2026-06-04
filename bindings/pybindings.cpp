61a8f2e80dc71de0712e60d3b6b44b75ef7a493b
salvat <tobias@avlas.cz>
2026-06-04 22:40:41 +0200

    Begin writing some simple pybind11 bindings

--- bindings/pybindings.cpp
+++ bindings/pybindings.cpp
@@ -1,14 +1,56 @@
 #include <pybind11/pybind11.h>
 
-#include <linalg/vector2.hpp>
+#include <linalg/Matrix.hpp>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(linalg, m)
-{
-    py::class_<Vector2>(m, "Vector2")
-        .def(py::init<float, float>())
-        .def("length", &Vector2::length)
-        .def_readwrite("x", &Vector2::x)
-        .def_readwrite("y", &Vector2::y);
+
+// template<size_t H, size_t W>
+// void bindIntMatrix(py::module_& m) {
+//     using M = Matrix<int, H, W>;
+//     py::class_<M>(m)
+//         .def(py::init<>())
+//         .def("get", &M::get)
+//         .def("set", &M::set)
+//         .def("__repr__", &M::print)
+//     ;
+// }
+
+void bindIntMatrix(py::module_& m) {
+    using M = Matrix<int, 2, 2>;
+    py::class_<M>(m)
+        .def(py::init<>())
+        // .def("get", &M::get)
+        .def("set", &M::set)
+        .def("__repr__", &M::print)
+    ;
+}
+
+// template<size_t... Ns>
+// void bind_vectors(py::module_& m, std::index_sequence<Ns...>) {
+//     (bind_vector<Ns + 1>(m,
+//         "Vector" + std::to_string(Ns + 1)), ...);
+// }
+
+PYBIND11_MODULE(linalg, m) {
+    m.doc() = "A simple library for basic matrix operations.";
+    //
+    // for (size_t i = 0; i < 20; ++i) {
+    //     for (size_t j = 0; j < 20; ++j) {
+    //         bindIntMatrix<i, j>(m);
+    //     }
+    // }
+    //
+    // bindIntMatrix<1,1>(m);
+    // bindIntMatrix<2,1>(m);
+    // bindIntMatrix<1,2>(m);
+    // bindIntMatrix<2,2>(m);
+
+    bindIntMatrix(m);
+    
+    // py::class_<Matrix>(m, Matrix);
+        // .def(py::init<float, float>())
+        // .def("length", &Vector2::length)
+        // .def_readwrite("x", &Vector2::x)
+        // .def_readwrite("y", &Vector2::y);
 }
